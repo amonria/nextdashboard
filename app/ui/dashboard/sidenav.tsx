00c2116961ebec14ce08816980969a2832643708
Krzysztof W <krzysztof.wylot@gmail.com>
2024-05-14 21:39:34 +0200

    update

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { useEffect } from 'react';
 
 export default function SideNav() {
+    
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
